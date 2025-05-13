5f31abca18058bd902bcb142f3e31be16d3c29f9
Architgupta225 <architgupta225@gmail.com>
2025-05-14 01:18:13 +0530

    User Model Created

---
+++ backend/src/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import authRoutes from "./routes/auth.route.js";
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
+
+const app = express();
+dotenv.config();
+
+const port = process.env.PORT;
+
+app.use("/api/auth", authRoutes);
+
+app.listen(port, () => {
+  console.log("Server is running on port:" + port);
+  connectDB()
+});
