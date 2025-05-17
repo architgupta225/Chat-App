aa913ccef1d93b6cb4ae5b13eb689e29af45a8aa
Architgupta225 <architgupta225@gmail.com>
2025-05-17 16:22:24 +0530

    Message routes

---
+++ backend/src/routes/message.route.js
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import {
+  getMessages,
+  getUsersforSidebar,
+  sendMessage,
+} from "../controllers/message.controller.js";
+
+const router = Router();
+
+router.get("/users", protectRoute, getUsersforSidebar);
+
+router.get("/:id", protectRoute, getMessages);
+
+router.post("/send/:id", protectRoute, sendMessage);
+
+export default router;
