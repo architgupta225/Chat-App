eb39cde9f8e881ec4c3eda91def54078731ad9e5
Architgupta225 <architgupta225@gmail.com>
2025-05-19 23:12:22 +0530

    Final commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,23 +2,25 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
 
-
-const app = express();
 dotenv.config();
 
 const port = process.env.PORT;
+
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
-app.use("/api/message", messageRoutes);
+app.use("/api/messages", messageRoutes);
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log("Server is running on port:" + port);
   connectDB();
 });
